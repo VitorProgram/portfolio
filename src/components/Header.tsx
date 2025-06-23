c287053db26b59f18262f3c81e03808db407ff8c
Vitor <joaovitornascimentoif@gmail.com>
2025-06-23 00:33:30 -0300

    feat: adding Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,51 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+const Header = () => {
+  const links = [
+    {
+      text: "Home",
+      path: "/#home",
+    },
+    {
+      text: "Work",
+      path: "/#work",
+    },
+    {
+      text: "About",
+      path: "/#about",
+    },
+  ];
+
+  const handleLogoClick = () => {
+    window.location.reload();
+  };
+
+  return (
+    <header className="w-full h-24 flex justify-between items-center">
+      <Button
+        variant="ghost"
+        className="p-0 cursor-pointer"
+        onClick={handleLogoClick}
+      >
+        <Image src="/logo.svg" alt="Vitor" width={120} height={75} />
+      </Button>
+
+      <nav className="flex gap-8">
+        {links.map((link, index) => (
+          <Link
+            key={index}
+            href={link.path}
+            className="font-semibold transition-colors duration-300 hover:text-primary"
+          >
+            {link.text}
+          </Link>
+        ))}
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
